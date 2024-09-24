3235fdd9841f9110adf84a80982c6f9b457fc259
Kauã <kaua.borba03@gmail.com>
2024-09-24 00:13:03 -0300

    Create: user map

--- TaskSystem/obj/Debug/net8.0/TaskSystem.AssemblyInfo.cs
+++ TaskSystem/obj/Debug/net8.0/TaskSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc974bd7e3c0338eef7433d4b17b0fb6c14821af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd303cf5b5420955fc47ca4c64a5dd7e3175e5b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
