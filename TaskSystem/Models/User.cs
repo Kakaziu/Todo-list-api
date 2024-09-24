05496a2260aa32beeb688b53829b10b4a091cb71
Kauã <kaua.borba03@gmail.com>
2024-09-23 23:22:05 -0300

    first commit

---
+++ TaskSystem/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskSystem.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+    }
+}
